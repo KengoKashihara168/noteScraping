fcc4f0147a61f8ac934f8f2aa7a3d5a46ec2db21
KengoKashihara <KengoKashihara1998@gmail.com>
2021-06-17 20:41:08 +0900

    JSONをオブジェクトに変換する関数を実装

--- main.js
+++ main.js
@@ -8,4 +8,7 @@ function myFunction() {
   // ダッシュボード情報の取得
   let dashboard = getDashboard(headers,2);
   console.log(dashboard.getContentText());
+
+  let obj = ConvertJsonToObject(dashboard);
+  console.log(obj["start_date_str"]);
 }
